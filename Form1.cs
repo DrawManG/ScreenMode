519f8a3a3f7147407f84c22e9e9a49792ba4a0e1
Дмитрий Смирнов <we-are-legion2013@yandex.ru>
2019-12-13 13:20:16 +0300

    FIXED VERS for git

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetDisplayMode(DisplayMode mode)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "DisplaySwitch.exe";
+            switch (mode)
+            {
+                case DisplayMode.External:
+                    proc.StartInfo.Arguments = " /external";
+                    break;
+                case DisplayMode.Internal:
+                    proc.StartInfo.Arguments = " /internal";
+                    break;
+                case DisplayMode.Extend:
+                    proc.StartInfo.Arguments = " /extend";
+                    break;
+                case DisplayMode.Duplicate:
+                    proc.StartInfo.Arguments = " /clone";
+                    break;
+            }
+            proc.Start();
+        }
+        enum DisplayMode
+        {
+            Internal,
+            External,
+            Extend,
+            Duplicate
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.Extend);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.Duplicate);
+        }
+    }
+}
